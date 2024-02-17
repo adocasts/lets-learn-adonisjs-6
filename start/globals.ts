66a23a4bbf0dceee46a26a6830362df5ecedf535
Tom Gobich <tom@tomgobich.com>
2024-02-17 08:22:05 -0500

    3.10 - EdgeJS Iconify

--- start/globals.ts
+++ start/globals.ts
@@ -1,3 +1,9 @@
 import edge from 'edge.js'
+import { edgeIconify, addCollection } from 'edge-iconify'
+import { icons as phIcons } from '@iconify-json/ph'
+
+addCollection(phIcons)
+
+edge.use(edgeIconify)
 
 edge.global('globalExample', 'Global Info')
