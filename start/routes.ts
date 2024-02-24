d378197c890992565c18963d6822ade387854900
Tom Gobich <tom@tomgobich.com>
2024-02-24 07:29:19 -0500

    adding example subdomain usage

--- start/routes.ts
+++ start/routes.ts
@@ -18,5 +18,28 @@ router
   .as('movies.show')
   .where('slug', router.matchers.slug())
 
+router
+  .get('/', async () => {
+    const builder = router
+      .builderForDomain('app.lvh.me')
+      .prefixUrl('http://app.lvh.me:3333')
+      .make('app.home')
+
+    const url = router.makeUrl(
+      'app.home',
+      {},
+      {
+        domain: 'app.lvh.me',
+        prefixUrl: 'http://app.lvh.me:3333',
+      }
+    )
+
+    console.log({ builder, url })
+
+    return 'route reached with subdomain'
+  })
+  .domain('app.lvh.me')
+  .as('app.home')
+
 router.delete('/redis/flush', [RedisController, 'flush']).as('redis.flush')
 router.delete('/redis/:slug', [RedisController, 'destroy']).as('redis.destroy')
