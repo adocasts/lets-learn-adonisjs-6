c38fde81803982cee602908c41c75e06f3c98041
Tom Gobich <tom@tomgobich.com>
2024-03-01 21:35:44 -0500

    4.8.2 - addon - adding environment specification to fake_seeder

--- database/seeders/fake_seeder.ts
+++ database/seeders/fake_seeder.ts
@@ -4,6 +4,8 @@ import { UserFactory } from '#database/factories/user_factory'
 import { BaseSeeder } from '@adonisjs/lucid/seeders'
 
 export default class extends BaseSeeder {
+  static environment = ['development']
+
   async run() {
     // Write your database queries inside the run method
     await CineastFactory.createMany(10)
