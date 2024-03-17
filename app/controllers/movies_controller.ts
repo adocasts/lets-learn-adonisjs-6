87f08f375f89ff67da9554ccda46d14892a7a68d
Tom Gobich <tom@tomgobich.com>
2024-03-17 10:08:26 -0400

    5.8 - Displaying A Movie's Writer and Director

--- app/controllers/movies_controller.ts
+++ app/controllers/movies_controller.ts
@@ -5,12 +5,16 @@ export default class MoviesController {
   async index({ view }: HttpContext) {
     const comingSoon = await Movie.query()
       .apply((scope) => scope.notReleased())
+      .preload('director')
+      .preload('writer')
       .whereNotNull('releasedAt')
       .orderBy('releasedAt')
       .limit(3)
 
     const recentlyReleased = await Movie.query()
       .apply((scope) => scope.released())
+      .preload('director')
+      .preload('writer')
       .orderBy('releasedAt', 'desc')
       .limit(9)
 
@@ -19,6 +23,10 @@ export default class MoviesController {
 
   async show({ view, params }: HttpContext) {
     const movie = await Movie.findByOrFail('slug', params.slug)
+
+    await movie.load('director')
+    await movie.load('writer')
+
     return view.render('pages/movies/show', { movie })
   }
 }
