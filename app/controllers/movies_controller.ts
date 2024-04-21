9888406ba398fa2d7c94056d6b93703ce10d1eb1
Tom Gobich <tom@tomgobich.com>
2024-04-21 09:29:12 -0400

    8.2 - Filter Our Movie List with a Movie Status Select

--- app/controllers/movies_controller.ts
+++ app/controllers/movies_controller.ts
@@ -1,17 +1,26 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import Movie from '#models/movie'
+import MovieStatus from '#models/movie_status'
 
 export default class MoviesController {
   async index({ request, view }: HttpContext) {
     const qs = request.qs()
     const movies = await Movie.query()
       .if(qs.search, (query) => query.whereILike('title', `%${qs.search}%`))
+      .if(qs.status, (query) => query.where('statusId', qs.status))
       .preload('director')
       .preload('writer')
+      .preload('status')
       .orderBy('title')
       .limit(15)
 
-    return view.render('pages/movies/index', { movies, filters: qs })
+    const movieStatuses = await MovieStatus.query().orderBy('name').select('id', 'name')
+
+    return view.render('pages/movies/index', {
+      movies,
+      movieStatuses,
+      filters: qs,
+    })
   }
 
   async show({ view, params }: HttpContext) {
