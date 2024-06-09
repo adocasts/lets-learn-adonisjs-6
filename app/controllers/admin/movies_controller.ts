89f5e2737129d2a4ddc4a5d6b54bb3d06ce45c51
Tom Gobich <tom@tomgobich.com>
2024-06-09 19:56:30 -0400

    11.8 - Posting an Array of Objects with a Native Form

--- app/controllers/admin/movies_controller.ts
+++ app/controllers/admin/movies_controller.ts
@@ -4,6 +4,7 @@ import { movieValidator } from '#validators/movie'
 import type { HttpContext } from '@adonisjs/core/http'
 import app from '@adonisjs/core/services/app'
 import router from '@adonisjs/core/services/router'
+import db from '@adonisjs/lucid/services/db'
 import { unlink } from 'node:fs/promises'
 
 export default class MoviesController {
@@ -60,9 +61,15 @@ export default class MoviesController {
   async edit({ view, params }: HttpContext) {
     const movie = await Movie.findOrFail(params.id)
     const data = await MovieService.getFormData()
+    const crewMembers = await db
+      .from('crew_movies')
+      .where('movie_id', movie.id)
+      .orderBy('sort_order')
+
     return view.render('pages/admin/movies/createOrEdit', {
       ...data,
       movie,
+      crewMembers,
     })
   }
 
@@ -70,7 +77,7 @@ export default class MoviesController {
    * Handle form submission for the edit action
    */
   async update({ params, request, response }: HttpContext) {
-    const { poster, ...data } = await request.validateUsing(movieValidator)
+    const { poster, crew, ...data } = await request.validateUsing(movieValidator)
     const movie = await Movie.findOrFail(params.id)
 
     if (poster) {
@@ -81,6 +88,16 @@ export default class MoviesController {
     }
 
     await movie.merge(data).save()
+
+    const crewMembers = crew?.reduce<Record<number, { title: string; sort_order: number }>>(
+      (acc, row, index) => {
+        acc[row.id] = { title: row.title, sort_order: index }
+        return acc
+      },
+      {}
+    )
+
+    await movie.related('crewMembers').sync(crewMembers ?? [])
 
     return response.redirect().toRoute('admin.movies.index')
   }
--- app/validators/movie.ts
+++ app/validators/movie.ts
@@ -22,5 +22,13 @@ export const movieValidator = vine.compile(
         if (!value) return
         return DateTime.fromJSDate(value)
       }),
+    crew: vine
+      .array(
+        vine.object({
+          id: vine.number().isExists({ table: 'cineasts', column: 'id' }),
+          title: vine.string(),
+        })
+      )
+      .optional(),
   })
 )
